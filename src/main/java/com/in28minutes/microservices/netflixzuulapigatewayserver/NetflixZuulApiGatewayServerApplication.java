dec002628d3b891a6c9cb1c382cf1638d566eb1b
esteban1225 <exteban.1225@gmail.com>
2020-06-04 23:07:58 -0500

    Api gateway server configuration

--- src/main/java/com/in28minutes/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
+++ src/main/java/com/in28minutes/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -2,7 +2,14 @@ package com.in28minutes.microservices.netflixzuulapigatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
+import brave.sampler.Sampler;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NetflixZuulApiGatewayServerApplication {
 
@@ -10,4 +17,8 @@ public class NetflixZuulApiGatewayServerApplication {
 		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
 	}
 
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
 }
